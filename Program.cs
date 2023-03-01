3440e8bbca12047e02fdad6de26362d6b5816966
Melissa Mayfield <106173451+Mayfieldmel@users.noreply.github.com>
2023-03-01 14:18:53 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+namespace CatWorx.BadgeMaker
+{
+    class Program
+    {
+        static void Main(string[] args) // entry point
+        {
+            // get employees
+            List<Employee> employees = GetEmployees();
+            // print employee
+            Util.PrintEmployees(employees);
+            Util.MakeCSV(employees);
+            Util.MakeBadges(employees);
+        }
+        static List<Employee> GetEmployees()
+        {
+            // create and initialize employee list
+            List<Employee> employees = new List<Employee>();
+            while (true)
+            {
+                // Prompt user
+                Console.WriteLine("Please enter a first name: (leave empty to exit: )");
+                // read input 
+                string firstName = Console.ReadLine() ?? "";
+                // exit loop
+                if (firstName == "")
+                {
+                    break;
+                }
+
+                Console.Write("Enter last name: ");
+                string lastName = Console.ReadLine() ?? "";
+                Console.Write("Enter ID: ");
+                int id = Int32.Parse(Console.ReadLine() ?? "");
+                Console.Write("Enter Photo URL: ");
+                string photoUrl = Console.ReadLine() ?? "";
+
+                // Employee Class Instance
+                Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
+                // add current employee to list
+                employees.Add(currentEmployee);
+            }
+            // return list of strings
+            return employees;
+        }
+    }
+}
+
+
---
+++ Util.cs
@@ -0,0 +1,50 @@
+
+// Import correct packages
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace CatWorx.BadgeMaker
+{
+    class Util
+    {
+        // Add List parameter to method
+        public static void PrintEmployees(List<Employee> employees)
+        {
+            for (int i = 0; i < employees.Count; i++)
+            {
+                string template = "{0,-10}\t{1,-20}\t{2}";
+                Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetFullName(), employees[i].GetPhotoUrl()));
+            }
+        }
+
+        public static void MakeCSV(List<Employee> employees)
+        {
+            // Check to see if folder exists
+            if (!Directory.Exists("data"))
+            {
+                // If not, create it
+                Directory.CreateDirectory("data");
+            }
+            // write to CSV file (employees.csv)
+            using (StreamWriter file = new StreamWriter("data/employees.csv"))
+            {
+                file.WriteLine("ID,Name,PhotoUrl");
+
+                // Loop over employees
+                for (int i = 0; i < employees.Count; i++)
+                {
+                    string template = "{0},{1},{2}";
+                    file.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetFullName(), employees[i].GetPhotoUrl()));
+                }
+            }
+        }
+
+        public static void MakeBadges(List<Employee> employees)
+        {
+            // Create image
+            SKImage newImage = SKImage.FromEncodedData(File.OpenRead("badge.png"));
+            SKData data = newImage.Encode();
+        }
+    }
+}
